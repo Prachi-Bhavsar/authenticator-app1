91f02e98ba71f9a070c01da216f2e65981743774
Prachi Bhavsar <prachicbhavsar@gmail.com>
2025-07-04 13:59:07 +0530

    Fixed root mapping and added /registerpage

--- src/main/java/com/controller/LoginauthenticatorController.java
+++ src/main/java/com/controller/LoginauthenticatorController.java
@@ -7,33 +7,35 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.entity.AuthorEntity;
 
 @Controller
-public class LoginauthenticatorController 
-{
-	@GetMapping("/loginpage")
-    public String showLoginPage() {
-        return "loginauth"; 
+public class LoginauthenticatorController {
+
+    // Root URL - show homepage
+    @GetMapping("/")
+    public String homePage() {
+        return "Homepage"; // Change this to your actual home view name
     }
-	
-	@GetMapping("/")
-	public String showRegistration()
-	{
-		return "register";
-	}
-	
-	@PostMapping("register")
-	public String registration(String fullname,String Attendence)
-	{
-		return "loginauth";
-	}
-	
-	@PostMapping("Saveauthor")
-	public String saveauthor(AuthorEntity authentity)
-	{
-		System.out.println(authentity.getFullname());
-		System.out.println(authentity.getEmail());
-		System.out.println(authentity.getUsername());
-		System.out.println(authentity.getPassword());
-		return "Homepage";
-	}
-	
+
+    @GetMapping("/loginpage")
+    public String showLoginPage() {
+        return "loginauth";
+    }
+
+    @GetMapping("/registerpage")
+    public String showRegistrationPage() {
+        return "register"; // This should match your register.jsp or register.html
+    }
+
+    @PostMapping("/register")
+    public String registration(String fullname, String Attendence) {
+        return "loginauth";
+    }
+
+    @PostMapping("/Saveauthor")
+    public String saveauthor(AuthorEntity authentity) {
+        System.out.println(authentity.getFullname());
+        System.out.println(authentity.getEmail());
+        System.out.println(authentity.getUsername());
+        System.out.println(authentity.getPassword());
+        return "Homepage";
+    }
 }
